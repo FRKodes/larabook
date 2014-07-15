3affc7c760b5793c1e210b9b9172bd9b479ca1b3
FRKodes <frkalderon@gmail.com>
2014-07-14 23:27:19 -0500

    Registration with BDD

---
+++ app/controllers/RegistrationController.php
@@ -0,0 +1,20 @@
+<?php
+
+class RegistrationController extends \BaseController {
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return View::make('registration.create');
+	}
+
+	public function store ()
+	{
+		return Redirect::home();
+	}
+
+}
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 	<div class="jumbotron">
-		<h1>Welcome to larabook</h1>
+		<h1>Welcome to larabook!</h1>
 		<p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
 		<p>
-			<a class="btn btn-lg btn-primary" href="/es/docs/bootstrap/examples/navbar-fixed-top/../../components/#navbar" role="button">View navbar docs &raquo;</a>
+			{{ link_to_route('register_path', 'Sign Up!', null, ['class' => 'btn btn-lg btn-primary']) }}
 		</p>
 	</div>
 @stop
